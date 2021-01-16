1efbe149277b30591b90bc3828afc22c5cee4bd5
Sebastien <sebastien@bossoutrot.com>
2021-01-16 16:48:35 +0100

    url with name and namespaces

--- leads/url.py
+++ leads/url.py
@@ -4,9 +4,9 @@ from leads.views import lead_list, lead_detail, lead_create, lead_update, lead_d
 app_name = "leads"
 
 urlpatterns = [
-    path('', lead_list),
-    path('create/', lead_create),
-    path('<int:pk>/update/', lead_update),
-    path('<int:pk>/delete/', lead_delete),
-    path('<int:pk>', lead_detail)
+    path('', lead_list, name = 'lead-list'),
+    path('create/', lead_create, name = 'lead-create'),
+    path('<int:pk>/update/', lead_update, name = 'lead-update'),
+    path('<int:pk>/delete/', lead_delete, name = 'lead-delete'),
+    path('<int:pk>', lead_detail, name = 'lead-detail')
 ]
