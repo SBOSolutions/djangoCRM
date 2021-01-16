f56f7d303058027e0a0a04a70c34cf42a885ea3a
Sebastien <sebastien@bossoutrot.com>
2021-01-16 16:18:06 +0100

    create django save method

--- leads/forms.py
+++ leads/forms.py
@@ -1,5 +1,21 @@
 from django import forms
+from django.db.models import fields
+from .models import Lead
 
+class LeadModelForm(forms.ModelForm):
+    class Meta:
+        model = Lead
+        fields = (
+            'first_name',
+            'last_name',
+            'age',
+            'agent'
+        )
+
+
+
+
+# for the record, only an example to explain haow it works
 class LeadForm(forms.Form):
     first_name = forms.CharField()
     last_name = forms.CharField()
